6ee80b23f2afa5fc6331d3fbd5942dd77b2da8cc
DeivysGrail <66143151+DeivysGrail@users.noreply.github.com>
2022-07-15 09:08:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,27 +11,27 @@ choix_01 = random.choice(cours_principal)
 choix_02 = random.choice(cours_pratique)
 choix_03 = random.choice(pratique_rapide)
 
-emploie_du_temps_chemin = Path.home() / "EDT" / "Schedule.txt"
+schedule_model = Path.home() / "Schedule.txt"
 
 
 # Semaine du ? --------------------------------------------------------
 unique_input = input("Semaine du lundi:")
 unique_input = f"Pour la semaine du lundi {unique_input}"
 
-# Création du fichier --------------------------------------------------
+# Création dossier + fichier  --------------------------------------------------
 schedule = Path.home() / "EDT"
-file_edt = schedule / f"{unique_input}.txt"
 schedule.mkdir(exist_ok=True)
-file_edt.touch()
+schedule_file = schedule / f"{unique_input}.txt"
+schedule_file.touch()
 
 # Modification du fichiers -------------------------------------------
-with open(emploie_du_temps_chemin, "r") as f:
+with open(schedule_model, "r") as f:
     texte = f.read()
     for i in texte:
         texte = texte.replace("MAIN", f"{random.choice(cours_principal)}")
         texte = texte.replace("PRATIQUE", f"{random.choice(cours_pratique)}")
         texte = texte.replace("PASSE_TEMPS", f"{random.choice(pratique_rapide)}")
-with open(file_edt, "w") as f:
+with open(schedule_file, "w") as f:
     f.write(texte)
 
 print(f"\033[32;1;20mVotre emploi du temps est prêt à être utilisé dans le dossier {schedule} !")
